8fe7c4a4d8b6300262f183c54b59abcc1568a5bf
Emjr31 <adonay.chacon12@gmail.com>
2021-10-26 15:36:58 -0600

    Creacion de las clases alumno y calificaciones

---
+++ G5-E2/alumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G5_E2
+{
+    class alumno
+    {
+        string carnet;
+        string nombres;
+        DateTime fecha;
+        string correo;
+        string responsable;
+
+        public string Carnet { get => carnet; set => carnet = value; }
+        public string Nombres { get => nombres; set => nombres = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Correo1 { get => correo; set => correo = value; }
+        public string Responsable { get => responsable; set => responsable = value; }
+    }
+}
---
+++ G5-E2/calificaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G5_E2
+{
+    class calificaciones
+    {
+        string carnet;
+        string materia;
+        double[] periodo= new double [3];
+
+        public string Carnet { get => carnet; set => carnet = value; }
+        public string Materia { get => materia; set => materia = value; }
+        public double[] Periodo { get => periodo; set => periodo = value; }
+    }
+}
